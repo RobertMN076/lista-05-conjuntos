c334b20eae290c292c8a34197adc271e344617e6
RobertMN076 <robertnobrega14@gmail.com>
2024-08-26 22:43:36 -0300

    questão 01

---
+++ tuplas.py
@@ -0,0 +1,13 @@
+#Questão 01
+
+informacoes = ('Ana', 20, 'RN')
+for infos in informacoes:
+    if infos == informacoes[0]:
+        print(f"Nome: {infos}")
+    elif infos == informacoes[1]:
+        print(f"Idade: {infos}")
+    else:
+        print(f"Estado: {infos}")
+
+#Questão 02
+
